04a50d5e3e63a852b7170b0d0619c5ebeead6682
Nick <nickitrytry@gmail.com>
2024-11-28 17:13:32 +0300

    added function and unary minus, broke toPostfix, calcPostfix

--- lab3_stack/lab3_stack/TCalc.cpp
+++ lab3_stack/lab3_stack/TCalc.cpp
@@ -1,26 +1,31 @@
 #include "TCalc.h"
 
-int TCalc::priority(char action)
+int TCalc::priority(Operation op)
 {
   int priority = 0;
-  switch (action)
+  switch (op)
   {
-  case '(':
-  case ')':
+  case OPN:
+  case CLS:
 	break;
-  case '-':
-  case '+':
+  case PLUS:
+  case MINUS:
 	priority = 1;
 	break;
-  case '*':
-  case '/':
+  case MULT:
+  case DIV:
 	priority = 2;
 	break;
-  case '^':
+  case POW:
 	priority = 3;
 	break;
+  case SIN:
+  case COS:
+  case EXP:
+	priority = 4;
+	break;
   default:
-	throw std::exception("not operation");
+	throw std::exception("isn't opertion");
 	break;
   }
   return priority;
@@ -31,7 +36,75 @@ bool TCalc::isOperation(char ch)
   return ch == '+' || ch == '-' || ch == '/' || ch == '*' || ch == '^';
 }
 
-TCalc::TCalc() :stNum(), stChar()
+bool TCalc::isFunction(const std::string str)
+{
+  return str == "sin" || str == "cos" || str == "exp";
+}
+
+Operation TCalc::getOperation(const std::string str)
+{
+  Operation op;
+  if (str == "+") op = PLUS;
+  else if (str == "-") op = MINUS;
+  else if (str == "*") op = MULT;
+  else if (str == "/") op = DIV;
+  else if (str == "^") op = POW;
+  else if (str == "sin") op = SIN;
+  else if (str == "cos") op = COS;
+  else if (str == "exp") op = EXP;
+  else throw std::exception("it's not operation");
+  return op;
+}
+
+double TCalc::calcOperation(Operation op)
+{
+  double res = 0.0;
+  double first = 0.0;
+  double second = 0.0;
+  switch (op)
+  {
+  case PLUS:
+	second = stNum.pop();
+	first = stNum.pop();
+	res = first + second;
+	break;
+  case MINUS:
+	second = stNum.pop();
+	first = stNum.pop();
+	res = first - second;
+	break;
+  case MULT:
+	second = stNum.pop();
+	first = stNum.pop();
+	res = first * second;
+	break;
+  case DIV:
+	second = stNum.pop();
+	first = stNum.pop();
+	res = first / second;
+	break;
+  case POW:
+	second = stNum.pop();
+	first = stNum.pop();
+	res = std::pow(first, second);
+	break;
+  case SIN:
+	first = stNum.pop();
+	res = std::sin(first);
+	break;
+  case COS:
+	first = stNum.pop();
+	res = std::cos(first);
+	break;
+  case EXP:
+	first = stNum.pop();
+	res = std::exp(first);
+	break;
+  }
+  return res;
+}
+
+TCalc::TCalc() :stNum(), stChar(), stOperation()
 {
   infix = "";
   postfix = "";
@@ -41,8 +114,9 @@ void TCalc::setInfix(std::string _infix)
 {
   infix = _infix;
   //временно
-  stNum = TStack<double>(infix.size() / 2);
-  stChar = TStack<char>(infix.size() / 2);
+  stNum = TStack<double>(infix.size());
+  stChar = TStack<char>(infix.size());
+  stOperation = TStack<Operation>(infix.size()/2);
 }
 
 std::string TCalc::getInfix()
@@ -60,40 +134,41 @@ std::string TCalc::getPostfix()
   return postfix;
 }
 
-void TCalc::toPostfix()
-{
-  std::string s = "(" + infix + ")";
-  for (int i = 0; i < s.size(); i++)
-  {
-	if (s[i] == '(')
-	  stChar.push(s[i]);
-	else if (s[i] == ')')
-	{
-	  char a = stChar.pop();
-	  while (a != '(')
-	  {
-		postfix += ' ';
-		postfix += a;
-		a = stChar.pop();
-	  }
-	}
-	else if (s[i] >= '0' && s[i] <= '9' || s[i] == '.')
-	{
-	  postfix += s[i];
-	  //postfix += ' ';
-	}
-	else if (isOperation(s[i]))
-	{
-	  postfix += ' ';
-	  while (priority(s[i]) <= priority(stChar.top()))
-	  {
-		postfix += stChar.pop();
-		postfix += ' ';
-	  }
-	  stChar.push(s[i]);
-	}
-  }
-}
+//void TCalc::toPostfix()
+//{
+//  std::string s = "(" + infix + ")";
+//  for (int i = 0; i < s.size(); i++)
+//  {
+//	if (s[i] == '(')
+//	  stChar.push(s[i]);
+//	else if (s[i] == ')')
+//	{
+//	  char a = stChar.pop();
+//	  while (a != '(')
+//	  {
+//		postfix += ' ';
+//		postfix += a;
+//		a = stChar.pop();
+//	  }
+//	}
+//	else if (s[i] >= '0' && s[i] <= '9' || s[i] == '.')
+//	{
+//	  postfix += s[i];
+//	  //postfix += ' ';
+//	}
+//	else if (isOperation(s[i]))
+//	{
+//	  postfix += ' ';
+//	  while (priority(s[i]) <= priority(stChar.top()))
+//	  {
+//		postfix += stChar.pop();
+//		postfix += ' ';
+//	  }
+//	  stChar.push(s[i]);
+//	}
+//  }
+//}
+
 
 double TCalc::postfixCalc()
 {
@@ -145,37 +220,24 @@ double TCalc::calc()
   std::string s = "(" + infix + ")";
   for (int i = 0; i < s.size(); i++) 
   {
-	if (s[i] == '(')
-	  stChar.push(s[i]);
+	if (s[i] == '(') {
+	  if (s[i + 1] == '-')
+	  {
+		std::size_t add;
+		stNum.push(std::stod(&(s[i+1]), &add));
+		i += add + 1; 
+	  }
+	  else 
+		stOperation.push(OPN);
+	}
 	else if (s[i] == ')')
 	{
-	  char a = stChar.pop();
-	  while (a != '(')
+	  Operation op = stOperation.pop();
+	  while (op != OPN)
 	  {
-		//подсчет операций
-		double secondNum = stNum.pop();
-		double firstNum = stNum.pop();
-		double res = 0;
-		switch (a)
-		{
-		case '+':
-		  res = firstNum + secondNum;
-		  break;
-		case '-':
-		  res = firstNum - secondNum;
-		  break;
-		case '*':
-		  res = firstNum * secondNum;
-		  break;
-		case '/':
-		  res = firstNum / secondNum;
-		  break;
-		case '^':
-		  res = pow(firstNum, secondNum);
-		  break;
-		}
+		double res = calcOperation(op);
 		stNum.push(res);
-		a = stChar.pop();
+		op = stOperation.pop();
 	  }
 	}
 	else if (s[i] >= '0' && s[i] <= '9')
@@ -186,32 +248,23 @@ double TCalc::calc()
 	}
 	else if (isOperation(s[i]))
 	{
-	  while (priority(s[i]) <= priority(stChar.top()))
+	  Operation op = getOperation(s.substr(i, 1));
+	  while (priority(op) <= priority(stOperation.top()))
 	  {
-		double secondNum = stNum.pop();
-		double firstNum = stNum.pop();
-		double res = 0;
-		switch (stChar.pop())
-		{
-		case '+':
-		  res = firstNum + secondNum;
-		  break;
-		case '-':
-		  res = firstNum - secondNum;
-		  break;
-		case '*':
-		  res = firstNum * secondNum;
-		  break;
-		case '/':
-		  res = firstNum / secondNum;
-		  break;
-		case '^':
-		  res = pow(firstNum, secondNum);
-		  break;
-		}
+		double res = calcOperation(stOperation.pop());
 		stNum.push(res);
 	  }
-	  stChar.push(s[i]);
+	  stOperation.push(op);
+	}
+	else if (isFunction(s.substr(i, 3)))
+	{
+	  Operation op = getOperation(s.substr(i, 3));
+	  while (priority(op) <= priority(stOperation.top()))
+	  {
+		double res = calcOperation(stOperation.pop());
+		stNum.push(res);
+	  }
+	  stOperation.push(op);
 	}
   }
   double res = stNum.pop();
--- lab3_stack/lab3_stack/TCalc.h
+++ lab3_stack/lab3_stack/TCalc.h
@@ -1,8 +1,23 @@
 #pragma once
 #include <string>
 #include "TStack.h"
+#include <cmath>
 
 //using namespace std;
+
+enum Operation
+{
+  OPN,
+  CLS,
+  PLUS,
+  MINUS,
+  MULT,
+  DIV,
+  POW,
+  SIN,
+  COS,
+  EXP
+};
 
 class TCalc {
   std::string infix;
@@ -10,9 +25,14 @@ class TCalc {
 
   TStack<double> stNum;
   TStack<char> stChar;
+  TStack<Operation> stOperation;
 
-  int priority(char action);
+  int priority(Operation op);
   bool isOperation(char ch);
+  bool isFunction(const std::string str);
+  Operation getOperation(const std::string str);
+  double calcOperation(Operation op);
+
 public:
   TCalc();
 
